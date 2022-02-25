fae7e68bd3cb3eaf45102d101401458e91d3a8cb
ryan <ryan@localhost>
2022-02-25 17:21:07 -0500

    initial config

---
+++ toasthub-core/src/test/java/org/toasthub/ToasthubCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toasthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToasthubCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
