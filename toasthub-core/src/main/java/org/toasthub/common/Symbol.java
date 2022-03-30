6e4df7f4caecf9ace491b2f29b35d74b05111509
RyanScout <ryanbeland02@gmail.com>
2022-03-30 15:34:46 -0400

    order configuration

---
+++ toasthub-core/src/main/java/org/toasthub/common/Symbol.java
@@ -0,0 +1,9 @@
+package org.toasthub.common;
+
+public class Symbol {
+    public static final String SPY = "SPY";
+    public static final String BTCUSD = "BTCUSD";
+    public static final String[] SYMBOLS = {Symbol.SPY , Symbol.BTCUSD};
+    public static final String[] CRYPTOSYMBOLS ={Symbol.BTCUSD};
+    public static final String[] STOCKSYMBOLS = {Symbol.SPY};
+}
